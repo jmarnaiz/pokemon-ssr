1ca2f2f1647fbfb5eb5d052d736055c751193ea5
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-06-25 12:50:36 +0200

    Modify server.ts to get it work for Netlify

--- src/server.ts
+++ src/server.ts
@@ -42,41 +42,41 @@ export async function netlifyCommonEngineHandler(
 /**
  * Serve static files from /browser
  */
-app.get(
-  '**',
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  })
-);
+// app.get(
+//   '**',
+//   express.static(browserDistFolder, {
+//     maxAge: '1y',
+//     index: 'index.html',
+//   })
+// );
 
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.get('**', (req, res, next) => {
-  const { protocol, originalUrl, baseUrl, headers } = req;
+// app.get('**', (req, res, next) => {
+//   const { protocol, originalUrl, baseUrl, headers } = req;
 
-  commonEngine
-    .render({
-      bootstrap,
-      documentFilePath: indexHtml,
-      url: `${protocol}://${headers.host}${originalUrl}`,
-      publicPath: browserDistFolder,
-      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-    })
-    .then((html) => res.send(html))
-    .catch((err) => next(err));
-});
+//   commonEngine
+//     .render({
+//       bootstrap,
+//       documentFilePath: indexHtml,
+//       url: `${protocol}://${headers.host}${originalUrl}`,
+//       publicPath: browserDistFolder,
+//       providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+//     })
+//     .then((html) => res.send(html))
+//     .catch((err) => next(err));
+// });
 
 /**
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+// if (isMainModule(import.meta.url)) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, () => {
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
 
-export default app;
+// export default app;
