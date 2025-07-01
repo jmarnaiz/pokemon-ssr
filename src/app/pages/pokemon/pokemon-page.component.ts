9fe664b60e5c873a7a8e3e30176fb165391c3c7e
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-07-01 11:14:07 +0200

    Add pokemon skeleton to pokemon page component

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -1,10 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import PokemonListComponent from '../../pokemon/components/pokemon-list/pokemon-list.component';
+import { PokemonListSkeletonComponent } from './ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 @Component({
   selector: 'pokemon-page',
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonPageComponent {}
+export default class PokemonPageComponent implements OnInit {
+  // public isLoading = signal(true);
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.isLoading.set(false);
+    // }, 5000);
+  }
+}
